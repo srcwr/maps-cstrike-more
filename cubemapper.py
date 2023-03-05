b5999009240ed40625825ff633da76f537d9365a
rtldg <rtldg@protonmail.com>
2023-03-05 03:08:47 +0000

    202303050250 + check .ain if no cubemap

--- cubemapper.py
+++ cubemapper.py
@@ -7,14 +7,22 @@ things = {}
 for filename in glob.iglob("filelist/*.csv"):
     with open(filename, newline='', encoding="utf-8") as f:
         cr = csv.reader(f)
+        shit = False
+        ain = ""
         for line in cr:
             if line[0] == "filename":
                 continue
-            if Path(line[0]).name == "cubemapdefault.vtf":
+            xd = Path(line[0])
+            if xd.name == "cubemapdefault.vtf":
                 a = Path(filename).stem
-                b = Path(Path(line[0]).parent).name
+                b = Path(xd.parent).name
                 things[a] = b
+                shit = True
                 break
+            if xd.suffix == ".ain":
+                ain = xd.stem
+        if not shit and ain != "":
+            things[Path(filename).stem] = ain
 
 with open("original_mapname.csv", "w", newline="", encoding="utf-8") as csvfile:
     mycsv = csv.writer(csvfile)
@@ -23,4 +31,4 @@ with open("original_mapname.csv", "w", newline="", encoding="utf-8") as csvfile:
     for k,v in xxx.items():
         mycsv.writerow([k, v])
 
-print(things)
+#print(things)
