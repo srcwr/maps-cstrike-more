c3a4af056c4d16d27d7657e59570cd321d86af17
rtldg <rtldg@protonmail.com>
2023-05-15 23:51:07 +0000

    202305152350

--- timestamper.py
+++ timestamper.py
@@ -1,7 +1,7 @@
 import glob
 import csv
 from pathlib import Path
-import datetime
+from datetime import datetime
 import os
 
 things = {}
@@ -11,18 +11,18 @@ with open("timestamps.csv", newline="", encoding="utf-8") as f:
     for line in cr:
         if line[0] == "sha1":
             continue
-        things[line[0]] = line[1]
+        things[line[0]] = (line[1], line[2])
 
 for filename in glob.iglob("../hashed/*.bsp"):
     hash = Path(filename).stem
     if not hash in things:
-        things[hash] = datetime.datetime.utcfromtimestamp(os.path.getmtime(filename))
+        things[hash] = (datetime.utcfromtimestamp(os.path.getmtime(filename)), datetime.utcfromtimestamp(os.path.getctime(filename)))
 
 with open("timestamps.csv", "w", newline="", encoding="utf-8") as csvfile:
     mycsv = csv.writer(csvfile)
-    mycsv.writerow(["sha1","timestamp"])
+    mycsv.writerow(["sha1","mod","create"])
     xxx = dict(sorted(things.items()))
     for k,v in xxx.items():
-        mycsv.writerow([k, v])
+        mycsv.writerow([k, v[0], v[1]])
 
 #print(things)
