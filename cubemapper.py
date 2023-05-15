7499856987499ea247d98a83cb90c069ccde7c10
rtldg <rtldg@protonmail.com>
2023-05-15 14:43:36 +0000

    don't read every csv

--- cubemapper.py
+++ cubemapper.py
@@ -4,7 +4,17 @@ from pathlib import Path
 
 things = {}
 
+with open("original_mapname.csv", newline="", encoding="utf-8") as f:
+    cr = csv.reader(f)
+    for line in cr:
+        if line[0] == "sha1":
+            continue
+        things[line[0]] = line[1]
+
 for filename in glob.iglob("filelist/*.csv"):
+    hash = Path(filename).stem
+    if hash in things:
+        continue
     with open(filename, newline='', encoding="utf-8") as f:
         cr = csv.reader(f)
         shit = False
@@ -14,15 +24,14 @@ for filename in glob.iglob("filelist/*.csv"):
                 continue
             xd = Path(line[0])
             if xd.name == "cubemapdefault.vtf":
-                a = Path(filename).stem
                 b = Path(xd.parent).name
-                things[a] = b
+                things[hash] = b
                 shit = True
                 break
             if xd.suffix == ".ain":
                 ain = xd.stem
         if not shit and ain != "":
-            things[Path(filename).stem] = ain
+            things[hash] = ain
 
 with open("original_mapname.csv", "w", newline="", encoding="utf-8") as csvfile:
     mycsv = csv.writer(csvfile)
