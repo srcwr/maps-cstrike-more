523df4a7764cb719d39b6c77e8bb00adda2e8bab
rtldg <rtldg@protonmail.com>
2024-04-06 01:15:35 +0000

    text/plain when uploading lump_checksums.csv

--- purge_venus.bat
+++ purge_venus.bat
@@ -1,4 +1,4 @@
-call wrangler r2 object put venus/lump_checksums.csv --file=lump_checksums.csv
+call wrangler r2 object put venus/lump_checksums.csv --file=lump_checksums.csv --content-type "text/plain"
 set /p PURGETOKEN=<..\secretpurge
 curl -X POST "https://api.cloudflare.com/client/v4/zones/1aa75e18589c3649abe7da1eb740bf46/purge_cache" ^
 	-H "Authorization: Bearer %PURGETOKEN%" ^
