9733c9569729278f815309ce7044eaf093cca078
rtldg <rtldg@protonmail.com>
2025-04-01 19:52:38 +0000

    make sure auto.cmd adds vscript_probably.csv

--- auto.cmd
+++ auto.cmd
@@ -4,7 +4,7 @@ uv run dumper.py
 uv run cubemapper.py
 uv run timestamper.py
 uv run lump_checksummer.py
-git add entitiesgz filelist ignore_pak.csv ignore.csv original_mapname.csv timestamps.csv lump_checksums.csv
+git add entitiesgz filelist ignore_pak.csv ignore.csv original_mapname.csv timestamps.csv lump_checksums.csv vscript_probably.csv
 SET GIT_COMMITTER_NAME= "srcwrbot"
 SET GIT_COMMITTER_EMAIL="bot@srcwr.com"
 git commit --author="srcwrbot <bot@srcwr.com>" -m "%1"
