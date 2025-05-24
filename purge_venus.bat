a9ed2e69dacf109b644cbdc16f0b8f8537ada38b
rtldg <rtldg@protonmail.com>
2025-05-24 01:22:47 +0000

    delete some unused scripts post maps-cstrike/mainframe

--- purge_venus.bat
+++
@@ -1,6 +0,0 @@
-call npx --yes wrangler r2 object put venus/lump_checksums.csv --file=lump_checksums.csv --content-type "text/plain" --remote
-set /p PURGETOKEN=<..\secretpurge
-curl -X POST "https://api.cloudflare.com/client/v4/zones/1aa75e18589c3649abe7da1eb740bf46/purge_cache" ^
-	-H "Authorization: Bearer %PURGETOKEN%" ^
-	-H "Content-Type: application/json" ^
-	--data "{\"files\":[\"https://venus.fastdl.me/lump_checksums.csv\"]}"
